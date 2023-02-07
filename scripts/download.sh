7d26c125cd2aab6d7d364559e6690dfe1f9cae67
Lukas Pirch <l.pirch@tu-bs.de>
2023-02-07 17:42:15 +0100

    fix missing parent dir in download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -13,6 +13,7 @@ done
 DATA_URL=https://bupteducn-my.sharepoint.com/:u:/g/personal/jackiecheng_bupt_edu_cn/EalnVAYC8zZDgwhPmGJ034cBYNZ8zB7-mNSNm-a7oYXkcw\?download\=1
 MODEL_URL=https://bupteducn-my.sharepoint.com/:u:/g/personal/jackiecheng_bupt_edu_cn/EesTvivx1UlEo9THYRSCYkMBMsZqKXgNVYx9wTToYnDwxg\?download\=1
 
+mkdir -p ${BASE_DIR}/data
 wget -O ${BASE_DIR}/data/data.7z ${DATA_URL}
 mkdir -p ${BASE_DIR}/models
 wget -O ${BASE_DIR}/models/dwk_pretrained.pt ${MODEL_URL}
