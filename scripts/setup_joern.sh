9377ef1ae5b7e5d2af4cd9d2e9881e5a4de638e0
Lukas Pirch <lukpirch@tu-bs.de>
2023-04-03 20:15:37 +0200

    fix joern setup

--- scripts/setup_joern.sh
+++ scripts/setup_joern.sh
@@ -15,7 +15,8 @@ tmp_dir="${BASE_DIR}/.dwk-preprocess"
 git clone $JOERN_URL ${tmp_dir}
 mv ${tmp_dir}/joern_slicer/joern ${BASE_DIR}
 rm -rf ${tmp_dir}
-
 pushd ${BASE_DIR}/joern
+chmod u+x build.sh
 ./build.sh
+chmod u+x joern-parse
 popd
